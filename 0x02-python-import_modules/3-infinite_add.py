f53b0a5ecf6ef4f541a3773b1840db0dfff21799
belyne <nzabonamarie@gmail.com>
2023-06-08 09:22:35 -0700

    Task3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+#from decimal import Decimal
+if __name__ == "__main__":
+    arguments = sys.argv[1:]
+    the_result = 0
+    for arg in arguments:
+        the_result += int(arg)
+    print(the_result)
