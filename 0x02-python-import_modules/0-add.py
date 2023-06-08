13c11e6f0998f44ecf5404d478543d5145419179
belyne <nzabonamarie@gmail.com>
2023-06-08 08:40:25 -0700

    Task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 from add_0 import add
 if __name__ == "__main__":
+    # Define the variables
     a = 1
     b = 2
-    result = add(1, 2)
+
+    # Call the add function from add_0 module
+    result = add(a, b)
+    # Print the result
     print("{} + {} = {}".format(a, b, result))
