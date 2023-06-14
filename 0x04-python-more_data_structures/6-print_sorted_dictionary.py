ec02ea6b8d31d03ed67e8f2361da4d71da63a297
belyne <nzabonamarie@gmail.com>
2023-06-14 07:17:51 -0700

    mandatory tasks

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,11 @@
 #!?usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    # Sort the keys of the dictionary in alphabetical order
     sorted_keys = sorted(a_dictionary.keys())
-
-    # Iterate over the sorted keys and print each key-value pair
     for key in sorted_keys:
-        print("{}: {}".format(key, a_dictionary[key]))
+        value = a_dictionary[key]
+        if isinstance(value, dict):
+            print("{}: {{".format(key))
+            print_sorted_dictionary(value)
+            print("}}")
+        else:
+            print("{}: {}".format(key, value))
