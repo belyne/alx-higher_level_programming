c647efdd68275fc2381927fcb17042e2ae036e37
belyne <nzabonamarie@gmail.com>
2023-07-12 11:01:06 -0700

    Loopup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""Defines looup function."""
+
+
 def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object.
+
+    Args:
+        obj: The object for which to retrieve the attributes and methods.
+
+    Returns:
+        A list of strings representing the attributes and methods of the object.
+    """
     return dir(obj)
