010e5ddc8c8e34cba5cfb5ce995e522a306d36b2
belyne <nzabonamarie@gmail.com>
2023-07-13 16:59:51 -0700

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,21 +1,7 @@
 #!/usr/bin/python3
+"""Defines class_to_json function."""
+
+
 def class_to_json(obj):
     """Returns the dictionary description of an object for JSON serialization."""
-    if isinstance(obj, dict):
-        return {k: class_to_json(v) for k, v in obj.items()}
-    elif isinstance(obj, list):
-        return [class_to_json(item) for item in obj]
-    elif isinstance(obj, str):
-        return obj
-    elif isinstance(obj, int):
-        return obj
-    elif isinstance(obj, bool):
-        return obj
-    else:
-        attributes = {}
-        for attr in dir(obj):
-            if not attr.startswith('__'):
-                value = getattr(obj, attr)
-                if not callable(value):
-                    attributes[attr] = class_to_json(value)
-        return attributes
+    return obj.__dict__
