47c6dc5cb8491111a60ec8a39debf1256ceaf25e
belyne <nzabonamarie@gmail.com>
2023-07-13 12:24:03 -0700

    Read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -14,5 +14,4 @@ def read_file(filename=""):
         None
     """
     with open(filename, "r", encoding="utf-8") as file:
-        content = file.read()
-        print(content)
+        print(file.read(), end="")
