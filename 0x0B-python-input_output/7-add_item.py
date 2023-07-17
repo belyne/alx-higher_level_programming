9cd948099e868e9312e8d8f604b80972cdc8a94b
belyne <nzabonamarie@gmail.com>
2023-07-17 00:00:18 -0700

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
-"""Adds all arguments to a Python list, and then save
-them to a file."""
+"""
+Adds all arguments to a Python list and saves them
+to a file in JSON format.
+"""
+
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+from typing import List
+from os import path
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
-if __name__ == "__main__":
-    # Load existing items from the file if it exists
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
+def add_to_list(filename: str, args: List[str]) -> List[str]:
+    """
+    Adds all arguments to a Python list and saves them
+    to a file in JSON format.
 
-    # Add new items from command line arguments
-    items.extend(sys.argv[1:])
+    Args:
+        filename (str): The name of the JSON file.
+        args (List[str]): The arguments to add to the list.
 
-    # Save the updated items to the file
-    save_to_json_file(items, "add_item.json")
+    Returns:
+        The updated list with the added arguments.
+    """
+    if path.exists(filename):
+        my_list = load_from_json_file(filename)
+    else:
+        my_list = []
+
+    my_list.extend(args)
+
+    save_to_json_file(my_list, filename)
+
+    return my_list
+
+
+if __name__ == '__main__':
+    filename = "add_item.json"
+    args = sys.argv[1:]
+    updated_list = add_to_list(filename, args)
+    print(updated_list)
