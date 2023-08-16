172c167a456b12cc248728a2a7aec73cd4c9070c
belyne <nzabonamarie@gmail.com>
2023-08-16 09:43:31 -0700

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,12 @@
+-- Task 4: First table
+-- Author: Belyne
+-- Date: Current date
+
+-- Use the specified database
+USE hbtn_0c_0;
+
+-- Create the "first_table" if it doesn't exist
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
