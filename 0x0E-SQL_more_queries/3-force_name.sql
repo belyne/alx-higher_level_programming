8e717a11c6df3090d1382252841b042b6cf07421
belyne <nzabonamarie@gmail.com>
2023-08-17 07:57:35 -0700

    Always a name

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,5 @@
+-- Create the table force_name with id INT and name VARCHAR(256) NOT NULL.
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
