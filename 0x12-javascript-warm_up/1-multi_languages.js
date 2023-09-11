defbbcbbbea7e6122d77c88da5337ebb54f4693c
belyne <nzabonamarie@gmail.com>
2023-09-11 04:52:49 -0700

    1. 3 languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+let line = 'C is fun'; // first line
+console.log(line);
+line = 'Python is cool'; // second line
+console.log(line);
+line = 'JavaScript is amazing'; // third line
+console.log(line);
