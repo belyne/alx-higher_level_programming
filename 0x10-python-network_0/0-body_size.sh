b1fd7c60141e9eb5fb68456914e416c28c7837e1
belyne <nzabonamarie@gmail.com>
2023-09-28 11:38:24 -0700

    Python -network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script takes in a URL, sends a request, and displays the size of the response body in bytes
 
-curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
+curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script sends a GET request to a URL and displays the body if the response status code is 200
 
-curl -s -X GET "$1" -i | grep "HTTP/1.0 200 OK" -q && curl -s -X GET "$1"
+curl -sL "$1"
